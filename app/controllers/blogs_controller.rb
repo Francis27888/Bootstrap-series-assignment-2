78fe906a47e1654ba16dc758abd3f7166732feaf
Kalisa athanase <kalisamukwiye@gmail.com>
2018-03-01 07:43:02 +0000

    rails series assignement 2

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,49 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show,:update,:destroy,:edit]
+  def index
+     @blogs=Blog.all
+  end
+  def confirm
+    @blog = Blog.new(blog_params)
+    render 'new' if @blog.invalid?
+  end
+  def new
+    if params[:back]
+      @blog=Blog.new(blog_params)
+    else
+       @blog=Blog.new
+    end
+  end
+  def create
+    @blog=Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "You have created new blog!"
+    else
+      render 'new'
+    end
+  end
+  def show
+    
+  end
+  def update
+    @blog=Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "The blog updated successfully!"
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path
+  end
+  
+  private 
+  def blog_params
+    params.require(:blog).permit(:name,:email,:content)
+  end
+  
+  def set_blog
+    @blog =Blog.find(params[:id])
+  end
+end
