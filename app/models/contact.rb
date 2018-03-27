a1a1cf7c5268e222989b923f428a00912d4e89b2
Kalisa athanase <kalisamukwiye@gmail.com>
2018-03-27 10:07:24 +0000

    removing uniqueness for email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :content,presence:true,length:{maximum:140}
 end
