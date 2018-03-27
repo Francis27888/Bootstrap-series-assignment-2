42fbc544ad44da34a14d86fd27a11ddbd41a1d1d
Kalisa athanase <kalisamukwiye@gmail.com>
2018-03-27 08:42:21 +0000

    Fixing contacts form design

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = Contact.all
+    @contacts = Contact.all.page(params[:page]).per(4)
   end
 
   # GET /contacts/1
