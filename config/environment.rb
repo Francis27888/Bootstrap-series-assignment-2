e1bcbf9440e3cb09d3af315a9620289b00a636ce
Kalisa athanase <kalisamukwiye@gmail.com>
2018-03-30 09:25:41 +0000

    sendgrid username

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => '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',
   :domain => 'bootstrap-series-assignment2.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
