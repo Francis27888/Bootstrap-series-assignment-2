253456b002aa9c385248a586bde05cd6d0ea9b24
Kalisa athanase <kalisamukwiye@gmail.com>
2018-03-30 09:03:07 +0000

    sendgrid username

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
+  :username => 'apikey',
   :password => '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',
   :domain => 'yourdomain.com',
   :address => 'smtp.sendgrid.net',
